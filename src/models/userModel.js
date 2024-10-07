4d2fdabe0996ad75cd129b683ab0fe698211eb6c
Elazar Porush <lzrprws@gmail.com>
2024-10-07 12:43:14 +0300

    begining of user model

---
+++ src/models/userModel.js
@@ -0,0 +1,21 @@
+const { type } = require('express/lib/response')
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    user_name: {
+        type: String,
+        required: [true, 'user name is required'],
+        minlength: [5, "way too short name, please enter at least 5 chars"]
+    },
+    password: {
+        type: String,
+        required: [true, "password must be provided"]
+    },
+    role:{
+        type: String,
+        enum: ['soldier', 'commander'],
+        required: [true, "please provide a role"]
+    }
+})
+
+const userModel = mongoose.model('user', userSchema)
